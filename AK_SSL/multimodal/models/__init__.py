51dc12d7e52d7599d61f1f1ec7cbdbc0fbb2b4ac
Kian Majlessi <kianmajl@gmail.com>
2024-07-08 12:21:13 +0330

    add simvlm method

--- AK_SSL/multimodal/models/__init__.py
+++ AK_SSL/multimodal/models/__init__.py
@@ -1,10 +1,12 @@
 from AK_SSL.multimodal.models.clip import CLIP
 from AK_SSL.multimodal.models.albef import ALBEF
 from AK_SSL.multimodal.models.slip import SLIP
+from AK_SSL.multimodal.models.simvlm import SimVLM
 
 
 __all__ = [
     "CLIP",
     "ALBEF",
     "SLIP",
+    "SimVLM",
 ]
