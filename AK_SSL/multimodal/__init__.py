33d0ae7afe5ca254d3a69861af136fbc63c5ee87
Audrina Ebrahimi <adrina.ei24@gmail.com>
2024-08-12 15:23:20 +0330

    Add init file for utils

--- AK_SSL/multimodal/__init__.py
+++ AK_SSL/multimodal/__init__.py
@@ -1,3 +1,3 @@
 from AK_SSL.multimodal.Trainer import Trainer
 
-__all__ = ['Trainer']
+__all__ = ["Trainer"]
---
+++ AK_SSL/multimodal/models/utils/simvlm/__init__.py
@@ -0,0 +1,3 @@
+from AK_SSL.multimodal.models.utils.simvlm.resblock import BottleneckBlock
+
+__all__ = ["BottleneckBlock"]
