35a957eb17aab7f78e8b20bcc0c30f1d2b23cf93
Audrina Ebrahimi <adrina.ei24@gmail.com>
2024-08-19 23:00:39 +0330

    Fix a bug in ALBEF model

--- AK_SSL/multimodal/models/albef.py
+++ AK_SSL/multimodal/models/albef.py
@@ -331,7 +331,7 @@ class ALBEF(nn.Module):
         self.queue_ptr[0] = ptr
 
     def get_feature_size(self, encoder: nn.Module):
-        encoder.to(self.device)
+        encoder = encoder.to(self.device)
         encoder.eval()
         dummy_input = torch.randn(1, 3, 32, 32)  # Create a dummy input for the encoder
         with torch.no_grad():
